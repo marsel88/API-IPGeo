8c93ff8d39915168108bea7c084da21aba15c386
Марсель <@->
2021-06-14 23:36:06 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IPGeo
+{
+    public class Root
+    {
+        public string country { get; set; }
+        public string country_phone { get; set; }
+        public string region { get; set; }
+        public string city { get; set; }
+        public string timezone { get; set; }
+        public string currency { get; set; }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace IPGeo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var IP = "46.224.227.20";
+            var url = $"https://ipwhois.app/json/{IP}?objects=country,city,region,country_phone,timezone,currency_code,currency";
+            
+
+            var request = WebRequest.Create(url);
+
+            var response = request.GetResponse();
+            var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+            if (httpStatusCode != HttpStatusCode.OK)
+            {
+                Console.WriteLine(httpStatusCode);
+                return;
+            }
+
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                string result = streamReader.ReadToEnd();
+                Console.WriteLine(result);
+                var weatherForecast = JsonConvert.DeserializeObject<Root>(result);
+                Console.WriteLine(weatherForecast.city);
+            }
+
+        }
+    }
+}
